d12cc20e87756aa06f9ac8645e722e1ae81fd992
Morten Jagd Christensen <mortenjc@jcaps.com>
2023-12-09 08:33:11 +0100

    day09 started late

--- day09/solution.py
+++ day09/solution.py
@@ -14,8 +14,27 @@ S2 = 0
 with open(infile) as fin:
     lines = ((fin.read().strip()).split('\n'))
 
+
 for l in lines:
-    print(l)
+    W = []
+    r = list(map(int, l.split()))
+    W.append(r)
+    while True:
+        tmp = []
+        for i in range(len(r)-1):
+            tmp.append(r[i+1]-r[i])
+        W.append(tmp)
+        r = tmp
+        if all([x == 0 for x in tmp]):
+            p = 0
+            p2a = 0
+            for l2 in W[::-1]:
+                p += l2[-1]
+                p2b = l2[0]
+                p2a = p2b - p2a
+            S1 += p
+            S2 += p2a
+            break
 
 
 
