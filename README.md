158af849a4aa343585b5ecc6d7d19acd037e51a0
Morten Jagd Christensen <mortenjc@jcaps.com>
2023-12-26 19:02:08 +0100

    day25 graph image

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # aoc2023
 Advent of code 2023
+
+Didn't finish it all but definitely got better than last year and learned
+a bunch.
+
+Here is a picture of the graphical solution of day25 - this would have been
+faster than my solution had I gotten the idea earlier.
+
+![day 25](day25/day25.png)
--- day25/solution2.py
+++ day25/solution2.py
@@ -15,13 +15,22 @@ with open(infile) as fin:
 
 E = defaultdict(set)
 G = nx.Graph()
+V = set()
+ni = {}
 for l in lines:
     par, chls = l.split(': ')
     chls = chls.split(' ')
+    V.add(par)
+    if not par in ni:
+        ni[par] = len(ni)
     for ch in chls:
+        if not ch in ni:
+            ni[ch] = len(ni)
         E[par].add(ch)
         E[ch].add(par)
         G.add_edge(par, ch, capacity=1.0)
+
+        V.add(ch)
 
 run = True
 for e1 in E:
@@ -33,9 +42,33 @@ for e1 in E:
         v, p = nx.minimum_cut(G, e1, e2)
         if v == 3:
             S1 = len(p[0]) * len(p[1])
+            se1 = e1
+            se2 = e2
+            print(se1, se2)
             run = False
             break
 
+
+from graphe.graph import graph
+from graphe.graph import bfs
+from graphe import draw
+
+MG = graph.Graph(len(V))
+SEEN = set()
+for adj in E:
+    for other in E[adj]:
+        a = ni[adj]
+        b = ni[other]
+        if (a,b) in SEEN:
+            continue
+        SEEN.add((a,b))
+        SEEN.add((b,a))
+        MG.add_edge(a, b)
+
+
+fig = draw.Draw()
+fig.node_attr(label='')
+fig.draw(MG)
 
 print("------------- A -------------")
 print('S1 ', S1)
